5a4a28c6a22ac29bd499de13d7dee1180aa5a03d
Kartik Kankurte <kartikkankurte@gmail.com>
2022-06-01 12:57:17 +0530

    add icons

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import '../stylesheets/Sidebar.css'
-import CommentRoundedIcon from '@mui/icons-material/CommentRounded';
+import Chat from '../icons/chat.png'
+import Donut from '../icons/Donut.png'
+import Dots from '../icons/dots.png'
 
 function Sidebar() {
     return (
         <div className='sidebar'>
             <div className='sidebar-header'>
                 <div className='sidebar-buttons'>
-                    <CommentRoundedIcon />
+                    <img src={Chat} alt='chat'></img>
+                    <img src={Donut} alt='chat'></img>
+                    <img src={Dots} height={25} alt='chat'></img>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
