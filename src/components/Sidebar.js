9e0bf750451b069349786ba1859ea255543351e2
Kartik Kankurte <kartikkankurte@gmail.com>
2022-06-01 15:57:49 +0530

    finished sidebar-header component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,18 +1,44 @@
 import React from 'react'
 import '../stylesheets/Sidebar.css'
-import Chat from '../icons/chat.png'
-import Donut from '../icons/Donut.png'
-import Dots from '../icons/dots.png'
+//icons from material-ui
+import { Avatar, IconButton } from '@mui/material'
+import ChatIcon from '@mui/icons-material/Chat';
+import DataSaverOffIcon from '@mui/icons-material/DataSaverOff';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchIcon from '@mui/icons-material/Search';
+import SidebarChat from './SidebarChat';
 
 function Sidebar() {
     return (
         <div className='sidebar'>
             <div className='sidebar-header'>
+                <Avatar src='https://avatars.githubusercontent.com/u/87076033?v=4' />
                 <div className='sidebar-buttons'>
-                    <img src={Chat} alt='chat'></img>
-                    <img src={Donut} alt='chat'></img>
-                    <img src={Dots} height={25} alt='chat'></img>
+                    <IconButton>
+                        <DataSaverOffIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
                 </div>
+            </div>
+
+            <div className='sidebar-search'>
+                <div className='search-container'>
+                    <input type='text' placeholder='Search...' />
+                    <SearchIcon />
+                </div>
+            </div>
+
+            <div className='sidebar-chats'>
+                <h2>All Chats</h2>
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
             </div>
         </div >
     )
